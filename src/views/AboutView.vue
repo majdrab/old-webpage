11de5f9ca94487cf10cb1f15de0fd10e20c04d0b
Maj Drab <maj.drab@gmail.com>
2022-11-10 16:13:13 +0100

    feat: create home and project views

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>hello</div> 
+    <MajIcon :icon="['fas', 'user']"/>
+</template>
+
+<script>
+// import Header from "./Components/Header"
+
+export default {
+  name: "AboutView",
+  components: {},
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
