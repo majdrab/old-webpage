dcb2767b54748fddd8e6502ae97df5a81a1277a0
Maj Drab <maj.drab@gmail.com>
2022-11-10 17:22:21 +0100

    chore: hide header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
+    <!-- <header>
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
         <div class="wrapper">
@@ -8,7 +8,7 @@
                 <RouterLink to="/about">About</RouterLink>
             </nav>
         </div>
-    </header>
+    </header> -->
     
     <NavigationBar />
     <RouterView />
