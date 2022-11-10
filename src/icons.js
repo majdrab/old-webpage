55a72ac0e52bbdfee9907a644933cc90615f9122
Maj Drab <maj.drab@gmail.com>
2022-11-10 11:06:06 +0100

    feat: update icons

---
+++ src/icons.js
@@ -0,0 +1,12 @@
+/**
+ * FontAwesome icons
+ */
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+
+import { fal } from "@fortawesome/pro-light-svg-icons"
+import { far } from "@fortawesome/pro-regular-svg-icons"
+import { fas } from "@fortawesome/pro-solid-svg-icons"
+import { fad } from "@fortawesome/pro-duotone-svg-icons"
+
+library.add(fal, far, fas, fad)
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+
+import "./icons"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app
+  .component("MajIcon", MajIcon)
+  // .component('ComponentB', ComponentB)
+  // .component('ComponentC', ComponentC)
+
+app.mount("#app")
