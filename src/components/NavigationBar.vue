4b6424e66c3479e2c7f2e74de0df98dde3c7158b
Maj Drab <maj.drab@gmail.com>
2022-11-14 11:57:36 +0100

    feat: make navigation bar, wip

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,20 +1,229 @@
 <template>
-    <nav>
-
+    <nav class="navigation">
+        <div class="navigation__container">
+            <div class="navigation__top">
+                <div class="navigation__logo">
+                    <MajDrabLogo @click="$router.push('/')" />
+                </div>
+            </div>
+            <div class="navigation__scroll-wrapper">
+                <div class="navigation__views">
+                    <router-link
+                        v-for="(link, index) in $options.links"
+                        :key="`link-${index}`"
+                        :title="link.title"
+                        :to="`/${link.route}`"
+                        @click="openNav = false"
+                    >
+                        <MajIcon :icon="link.icon" :type="iconStyle(`/${link.route}`)"/>
+                        <span>{{ link.title }}</span>
+                    </router-link>
+                </div>
+            </div>
+            <div class="navigation__bottom">
+                <a 
+                    href="https://github.com/majdrab/majdrab.github.io"
+                    title="Github Repo"
+                >
+                    <MajIcon :icon="['fab', 'github']"/>
+                    <span>GitHub Repo</span>
+                </a>
+            </div>
+        </div>
     </nav>
 </template>
 
 <script>
-
+import MajDrabLogo from "@/assets/MajDrabLogo.vue"
 
 export default {
-  data () {
-    return {
-    
+  links: [
+    {
+      route: "",
+      title: "Home",
+      icon: "house"
+    },
+    {
+      route: "about",
+      title: "About",
+      icon: "user"
+    },
+    {
+      route: "projects",
+      title: "Projects",
+      icon: "star-sharp"
+    }
+  ],
+  // data () {
+  //   return {
+  //   }
+  // },
+  components: { MajDrabLogo },
+  methods: {
+    iconStyle (path, exact) {
+      let style = "far"
+      if (
+        (exact && this.$route.path === path) ||
+        (!exact && this.$route.path.startsWith(path))
+      ) {
+        style = "fas"
+      }
+      console.log(exact)
+      return style
     }
   }
 }
 </script>
 
 <style lang="scss">
+.navigation {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 100vh;
+  width: 100%;
+  max-width: 260px;
+  transition: left 0.5s, max-width 0.3s ease-out;
+  .navigation__container {
+    width: 100%;
+    height: 100%;
+    background: $bgDark;
+    background: linear-gradient(0deg, $bgDark, $bgDarkLighter);
+    flex-grow: 1;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    .navigation__top {
+      width: 100%;
+      .navigation__logo {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        padding: 0 25px 0 20px;
+        height: $headerHeightDefault;
+        position: relative;
+        svg {
+          height: 17px;
+          cursor: pointer;
+        }
+      }
+    }
+    .navigation__scroll-wrapper {
+      overflow: auto;
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: flex-start;
+      width: 100%;
+      padding: 10px 0;
+      flex-grow: 1;
+      // &::-webkit-scrollbar-track {
+      //   display: initial;
+      // }
+      // &::-webkit-scrollbar {
+      //   display: initial;
+      //   border-left: none;
+      // }
+      // &::-webkit-scrollbar-thumb {
+      //   background: rgba($textSecondary, 0.4);
+      //   border: none;
+      //   &:hover {
+      //     background: rgba($textSecondary, 0.5);
+      //   }
+      //   &:active {
+      //     background: rgba($textSecondary, 0.6);
+      //   }
+      // }
+    }
+    .navigation__views,
+    .navigation__bottom {
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: flex-start;
+      width: 100%;
+      a,
+      button {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 16px 25px;
+        text-decoration: none;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 1.2;
+        width: 100%;
+        border-radius: 0;
+        border: none;
+        outline: none;
+        background: transparent;
+        transition: background-color 0.1s, transform 0.1s;
+        border-left: solid 0px transparent;
+        position: relative;
+        svg {
+          font-size: 16px;
+          width: 18px;
+          transition: color 0.1s;
+          color: rgba($textSecondary, 0.8);
+        }
+        span {
+          color: $textSecondary;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          max-width: 100%;
+          margin-left: 12px;
+          transition: all 0.1s;
+        }
+        &:hover {
+          transition: all 0.05s;
+          cursor: pointer;
+          background: rgba(#3b3b3f, 0.6);
+          svg {
+            color: rgba(white, 0.75);
+            transition: all 0.05s;
+          }
+          span {
+            color: white;
+            transition: all 0.05s;
+          }
+        }
+        &:active {
+          transition: all 0.1s;
+          background: rgba(#3b3b3f, 0.8);
+        }
+        &.router-link-active,
+        &.router-link-exact-active {
+          background: rgba(#3b3b3f, 0.6);
+          svg {
+            color: lighten($primary, 2%);
+            animation: navigation-active-icon 0.2s;
+            @keyframes navigation-active-icon {
+              from {
+                opacity: 0.5;
+                transform: scale(0.6);
+              }
+            }
+          }
+          span {
+            color: white;
+          }
+        }
+        & + a,
+        & + button {
+          margin-top: 1px;
+        }
+      }
+    }
+    .navigation__views {
+      flex-grow: 1;
+      padding-bottom: 3px;
+    }
+  }
+}
 </style>
