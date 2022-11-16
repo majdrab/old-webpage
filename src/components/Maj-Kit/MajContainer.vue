c36fc28f57f4dd23a37cc7270199016555afe669
Maj Drab <maj.drab@gmail.com>
2022-11-16 14:14:53 +0100

    feat: add a container component

---
+++ src/components/Maj-Kit/MajContainer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div :class="[
+        'maj-container maj-container--' + ( horizontal ? 'horizontal' : 'vertical'),
+        {'maj-container--centerX': centerX},
+        {'maj-container--centerY': centerY}
+    ]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MajContainer",
+  props: {
+    horizontal: { type: Boolean, default: false },
+    centerX: { type: Boolean, default: false },
+    centerY: { type: Boolean, default: false }
+  }
+}
+</script>
+
+<style lang="scss">
+.maj-container {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-grow: 1;
+
+  &--vertical {
+    flex-direction: column;
+  }
+  &--horizontal {
+    flex-direction: row;
+  }
+  &--centerX {
+    align-items: center;
+  }
+  &--centerY {
+    justify-content: center;
+  }
+}
+</style>
