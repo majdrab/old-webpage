545fb373221115b61eb92df45ee7145760511a03
Maj Drab <maj.drab@gmail.com>
2022-11-21 11:30:55 +0100

    feat: add title and paragraph components

---
+++ src/components/Maj-Kit/MajParagraph.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="maj-paragraph" :style="{'margin-top: 20px;' : margin}" >
+        <slot></slot>
+    </p>
+</template>
+
+<script>
+export default {
+  name: "MajParagraph",
+  props: {
+    margin: { type: Boolean, default: false }
+  }
+}
+</script>
+
+<style lang="scss">
+p.maj-paragraph {
+  font-size: 1.2rem;
+  line-height: 1.3rem;
+  font-weight: 400;
+}
+</style>
---
+++ src/components/Maj-Kit/MajTitle.vue
@@ -0,0 +1,37 @@
+<template>
+    <component :is="is" class="maj-title">
+        <slot></slot>
+    </component>
+</template>
+
+<script>
+export default {
+  name: "MajTitle",
+  props: {
+    is: { type: String, default: "h3" },
+  }
+}
+</script>
+
+<style lang="scss">
+h1.maj-title  {
+  font-size: 5rem;
+  line-height: 6rem;
+  font-weight: 600;
+}
+h2.maj-title  {
+  font-size: 3rem;
+  line-height: 3.6rem;
+  font-weight: 500;
+}
+h3.maj-title  {
+  font-size: 2rem;
+  line-height: 2.4rem;
+  font-weight: 500;
+}
+h4.maj-title  {
+  font-size: 1.8rem;
+  line-height: 2.16rem;
+  font-weight: 500;
+}
+</style>
