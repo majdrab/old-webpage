e903e918c3e6259294e7983a1b115f9d86734974
Maj Drab <maj.drab@gmail.com>
2022-11-21 15:30:32 +0100

    feat: make options for image links, computed and method for passing src to img element

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,67 @@
 <template>
-    <div>hello</div> 
-    <MajIcon :icon="['fas', 'user']"/>
+    <div class="about">
+        <MajContainer class="about__section">
+            <img class="about__section-img" :src="getImage" alt="My pictures" @click="nextImage()">
+            <!-- <MajParagraph>
+                I'm Maj Drab, a 28-year-old Slovenian junior Front-end developer. Currently in the process of gaining more programming knowledge and doing all kinds of projects.
+            </MajParagraph> -->
+            <MajParagraph>
+                {{ getImage }}
+            </MajParagraph>
+        </MajContainer>
+    </div>
 </template>
 
 <script>
-// import Header from "./Components/Header"
-
 export default {
   name: "AboutView",
-  components: {},
-  data() {
+  image_links: [
+    "./images/me-1.jpg",
+    "./images/me-2.jpg",
+    "./images/me-3.jpg"
+  ],
+  data () {
     return {
+      picIndex: 0
     }
   },
   computed: {
+    getImage () {
+      return this.$options.image_links[this.picIndex]
+    }
   },
+  // created () {
+  //   this.nextImage()
+  // },
   methods: {
+    nextImage () {
+      if (this.picIndex < this.$options.image_links.length - 1) {
+        this.picIndex += 1
+      } else {
+        this.picIndex = 0
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.about {
+  width: calc(100vw - 200px);
+  height: 100vh;
+  
+  &__section {
+    max-width: 1000px;
+    display: flex;
+    & > * {
+      flex: 1 1 0px;
+    }
+
+    &-img {
+      width: 300px;
+      // max-width: ;
+    }
+  }
+}
 
 </style>
