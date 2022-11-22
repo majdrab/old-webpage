95fa4b9d1d2980f890af96d27815c86b36e234be
Maj Drab <maj.drab@gmail.com>
2022-11-22 16:48:51 +0100

    feat: project view iterates over projects json file which creates project preview buttons

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,25 +1,40 @@
 <template>
-    <div>hello</div> 
-    <MajIcon :icon="['fas', 'user']"/>
+    <MajContainer class="projects">
+        <SingleProject
+            v-for="(project, index) in projects"
+            :key="index"
+            :project="project"
+        />
+    </MajContainer>
 </template>
 
 <script>
-// import Header from "./Components/Header"
+import SingleProject from "@/components/SingleProject.vue"
+import projectsData from "@/data/projectsData.json"
 
 export default {
   name: "ProjectsView",
-  components: {},
+  components: { SingleProject },
   data() {
     return {
+      projects: projectsData
     }
-  },
-  computed: {
-  },
-  methods: {
   }
+  // computed: {
+  // },
+  // methods: {
+  // }
 }
 </script>
 
 <style lang="scss">
-
+.projects {
+  width: calc(100vw - 200px);
+  height: 100vh;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 30px;
+  align-content: flex-start;
+  overflow-y: auto;
+}
 </style>
