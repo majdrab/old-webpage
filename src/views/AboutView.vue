27b48849d2cfecf39f66551d2c6c0b2e8472b98b
Maj Drab <maj.drab@gmail.com>
2022-11-24 09:40:25 +0100

    feat: organise base about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,62 @@
 <template>
-    <div class="about">
+    <MajContainer vertical class="about">
         <MajContainer class="about__section">
-            <img class="about__section-img" :src="getImage" alt="My pictures" @click="nextImage()">
-            <!-- <MajParagraph>
-                I'm Maj Drab, a 28-year-old Slovenian junior Front-end developer. Currently in the process of gaining more programming knowledge and doing all kinds of projects.
-            </MajParagraph> -->
-            <MajParagraph>
-                {{ getImage }}
-            </MajParagraph>
+            <MajContainer class="about__section-sub">
+                <img class="about__my-img" :src="getImage" alt="My pictures" @click="nextImage()">
+            </MajContainer>
+            <MajContainer vertical class="about__section-sub">
+                <MajTitle is="h4">
+                    About Me.
+                </MajTitle>
+                <MajParagraph>
+                    I'm Maj Drab, a 28-year-old Slovenian junior Front-end developer. Currently in the process of gaining more programming knowledge and doing all kinds of projects.
+                </MajParagraph>
+                <MajParagraph>
+                    I like to resolve design problems, create smart user interface and imagine useful interaction, developing rich web experiences & web applications.
+                    When not working or futzing around with code, I like to play D&D, video/board games, or go for a walk in the woods.
+                </MajParagraph>
+                <MajParagraph>
+                    I'm actively looking for a job.
+                </MajParagraph>
+            </MajContainer>
         </MajContainer>
-    </div>
+        <MajContainer class="about__section">
+            <MajContainer vertical class="about__section-sub">
+                <MajTitle is="h4">
+                    Familiar with:
+                </MajTitle>
+                <MajContainer>
+                    <MajIcon :icon="['fab', 'vuejs']"/>
+                </MajContainer>
+                
+            </MajContainer>
+            <MajContainer vertical class="about__section-sub">
+                <MajTitle is="h4">
+                    Getting to know:
+                </MajTitle>
+                <MajContainer>
+                    <MajIcon :icon="['fab', 'react']"/>
+                </MajContainer>
+            </MajContainer>
+        </MajContainer>
+        <MajContainer vertical class="about__section">
+            <MajTitle is="h4">
+                My Udemy Certificates
+            </MajTitle>
+            <MajContainer class="about__certificates">
+                <div 
+                    v-for="(certificate, index) in $options.certificate_links"
+                    :key="index"
+                    class="about__certificates-wrapper"
+                >
+                    <img
+                        :src="$options.certificate_links[index]"
+                        alt="certificate"
+                    />
+                </div>
+            </MajContainer>
+        </MajContainer>
+    </MajContainer>
 </template>
 
 <script>
@@ -19,6 +66,14 @@ export default {
     "./images/me-1.jpg",
     "./images/me-2.jpg",
     "./images/me-3.jpg"
+  ],
+  icons: [
+    "vuejs",
+    "./images/certificates/web-design-for-beginners.jpg"
+  ],
+  certificate_links: [
+    "./images/certificates/git-github.jpg",
+    "./images/certificates/web-design-for-beginners.jpg"
   ],
   data () {
     return {
@@ -49,19 +104,52 @@ export default {
 .about {
   width: calc(100vw - 200px);
   height: 100vh;
+  padding: 30px;
+  gap: 40px;
+  overflow-y: auto;
   
   &__section {
     max-width: 1000px;
-    display: flex;
-    & > * {
+    width: 100%;
+    max-height: 50vh;
+    margin: 0 auto;
+    gap: 20px;
+    &-sub {
       flex: 1 1 0px;
+      gap: 20px;
+      .about__my-img {
+        width: 100%;
+        aspect-ratio: 1/1;
+        // aspect-ratio: 500/375;
+        object-fit: cover;
+        object-position: 50% 50%;
+        border-radius: 50%;
+      }
+      .maj-paragraph {
+        color: $textDarkIsh;
+        text-align: justify;
+      }
+      .maj-icon {
+        color: $textDarkIsh;
+        font-size: 4rem;
+      }
     }
+    .maj-title {
+      color: $textDark;
+    }
+    .about__certificates {
+      flex-wrap: wrap;
+      gap: 20px;
+      align-content: flex-start;
 
-    &-img {
-      width: 300px;
-      // max-width: ;
+      &-wrapper {
+        max-width: 450px;
+        // width: 100%;
+        img {
+          width: 100%;
+        }
+      }
     }
   }
 }
-
 </style>
