ea34759306084250bc2e7745f92e60e68be64d24
Maj Drab <maj.drab@gmail.com>
2022-11-29 16:57:06 +0100

    feat: add svg, menu icon and transitions to dice menu

--- src/components/MenuDice.vue
+++ src/components/MenuDice.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="menu-dice"></div>
+    <div class="menu-dice">
+        <MajContainer centerX centerY class="menu-dice__base">
+            <MajIcon class="menu-dice__base-icon" :icon="['fas', 'bars']" />
+        </MajContainer>
+    </div>
 </template>
 
 <script>
@@ -11,8 +15,29 @@ export default {
 
 <style lang="scss" scoped>
 .menu-dice {
-  width: 200px;
-  height: 200px;
-  background: $beige;
+  min-width: 80px;
+  height: 80px;
+  position: relative;
+  z-index: 2;
+
+  &__base {
+    min-width: 80px;
+    height: 80px;
+    position: fixed;
+    top: -20px;
+    background: no-repeat url('./images/bg/d20.svg');
+    background-size: 80px 80px;
+    opacity: 0.7;
+    transition: all 0.3s;
+    &-icon {
+      color: $primary;
+      font-size: 1.3rem;
+    }
+
+    &:hover {
+      opacity: 1;
+      transform: translateY(30px) scale(1.2);
+    }
+  }
 }
 </style>
