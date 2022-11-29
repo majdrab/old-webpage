fc28c3b25744aa95c003322266d71f8d8dfa90b8
Maj Drab <maj.drab@gmail.com>
2022-11-29 12:54:49 +0100

    feat: create base component for compressed menu

---
+++ src/components/MenuDice.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="menu-dice"></div>
+</template>
+
+<script>
+export default {
+  name: "MenuDice"
+}
+</script>
+
+
+<style lang="scss" scoped>
+.menu-dice {
+  width: 200px;
+  height: 200px;
+  background: $beige;
+}
+</style>
