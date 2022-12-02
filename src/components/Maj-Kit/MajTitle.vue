e7c5a4ad0a95a1a9707ee62ffa03c170f2ab9d2c
Maj Drab <maj.drab@gmail.com>
2022-12-02 12:31:37 +0100

    feat: reduce title sizes

--- src/components/Maj-Kit/MajTitle.vue
+++ src/components/Maj-Kit/MajTitle.vue
@@ -15,23 +15,23 @@ export default {
 
 <style lang="scss">
 h1.maj-title  {
-  font-size: 5rem;
-  line-height: 6rem;
-  font-weight: 600;
-}
-h2.maj-title  {
   font-size: 3rem;
   line-height: 3.6rem;
   font-weight: 500;
 }
-h3.maj-title  {
+h2.maj-title  {
   font-size: 2rem;
   line-height: 2.4rem;
   font-weight: 500;
 }
-h4.maj-title  {
+h3.maj-title  {
   font-size: 1.6rem;
   line-height: 1.92rem;
+  font-weight: 400;
+}
+h4.maj-title  {
+  font-size: 1.5rem;
+  line-height: 1.8rem;
   font-weight: 500;
 }
 </style>
