d5287c074a98303bd8787ff98cb4ef4f38091395
Maj Drab <maj.drab@gmail.com>
2022-12-02 13:55:37 +0100

    feat: rename singleProject to ProjectThumbnail and make many modifications to the component

---
+++ src/components/ProjectThumbnail.vue
@@ -0,0 +1,75 @@
+<template>
+    <button class="project-thumbnail">
+        <MajContainer centerY class="project-thumbnail__top">
+            <img
+                v-if="project.image"
+                :src="project.image"
+                alt="Project preview image"
+            >
+            <ProjectThumbnailSvg 
+                :id="project.id"
+            />
+        </MajContainer>
+        <MajContainer centerX class="project-thumbnail__bottom">
+            <MajTitle is="h3" class="project-thumbnail__bottom-name">{{ project.name }}</MajTitle>
+        </MajContainer>
+    </button>
+</template>
+
+<script>
+import ProjectThumbnailSvg from "./ProjectThumbnailSvg.vue";
+
+
+export default {
+  name: "ProjectThumbnail",
+  components: { ProjectThumbnailSvg },
+  props: {
+    project: { type: Object, default: () => {} }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.project-thumbnail {
+  max-width: 500px;
+  aspect-ratio: 5 / 4;
+  width: 100%;
+  cursor: pointer;
+  overflow: hidden;
+  background: $primary;
+
+  &__top {
+    width: 100%;
+    height: 70%;
+    position: relative;
+    .project-svg-0 {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 30px;
+      width: 100%;
+    }
+  }
+  &__bottom {
+    width: 100%;
+    height: 30%;
+    position: relative;
+    background: $light;
+    padding: 0 50px;
+
+    &-name {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      color: $primary;
+    }
+  }
+  &:hover {
+    .project-thumbnail-svg__secondary {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+}
+</style>
