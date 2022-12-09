7a3c8da4cea9ce3618281e5cb622b350fe9afdd2
Maj Drab <maj.drab@gmail.com>
2022-12-09 12:50:06 +0100

    feat: create component for containing links to my pages

---
+++ src/components/footer/ContactLinks.vue
@@ -0,0 +1,56 @@
+<template>
+    <MajContainer centerY class="contact-links">
+        <a
+            v-for="(contact, index) in $options.contact_links"
+            :key="`contact-${index}`"
+            class="contact-links__contact"
+            :title="contact.title"
+            :href="contact.href" 
+            target="_blank"
+        >
+            <MajIcon :icon="['fab', contact.icon]"/>
+        </a>
+    </MajContainer>
+</template>
+
+<script>
+export default {
+  name: "ContactLinks",
+  contact_links: [
+    {
+      href: "https://www.facebook.com/maj.drab/",
+      title: "Facebook",
+      icon: "facebook"
+    },
+    {
+      href: "https://www.linkedin.com/in/maj-drab-891a1041/",
+      title: "LinkedIn",
+      icon: "linkedin"
+    },
+    {
+      href: "https://github.com/majdrab",
+      title: "GitHub",
+      icon: "github"
+    }
+  ]
+}
+</script>
+
+<style lang="scss">
+.contact-links {
+  gap: 20px;
+  position: absolute;
+  z-index: 2;
+  &__contact {
+    color: $lPrimary;
+    font-size: 2rem;
+    transition: all 0.2s;
+    
+    &:hover {
+      cursor: pointer;
+      color: $secondary;
+      scale: 1.05;
+    }
+  }
+}
+</style>
