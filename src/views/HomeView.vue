62aa3ae1f20b5651e1afde73873cc04b3c01503f
Maj Drab <maj.drab@gmail.com>
2022-12-14 15:13:01 +0100

    feat: remove svg animation from home page and add name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,18 @@
 <template>
     <MajContainer class="home" centerX centerY>
-        <MajContainer class="home__container" >
-            <MajDrabLogo class="home__container-logo" />
-            <MajContainer vertical class="home__container-description">
-                <MajTitle is="h4">I'm a junior front-end web developer and designer. Welcome to my portfolio website!</MajTitle>
-                <MajTitle is="h4">The website is still a work in progress. I hope you'll still enjoy it!</MajTitle>
+        <MajContainer vertical class="home__container" >
+            <MajTitle is="h1">Maj Drab</MajTitle>
+            <MajContainer vertical class="home__desc">
+                <MajParagraph size="1.6">I'm a junior front-end web developer and designer. Welcome to my portfolio website!<br><br>The website is still a work in progress. I hope you'll still enjoy it!</MajParagraph>
             </MajContainer>
         </MajContainer>
     </MajContainer>
 </template>
 
 <script>
-import MajDrabLogo from "@/assets/MajDrabLogo.vue"
 
 export default {
   name: "HomeView",
-  components: {MajDrabLogo},
   data() {
     return {
     }
@@ -30,36 +27,22 @@ export default {
 <style lang="scss">
 .home {
   width: 100vw;
-  padding: 50px 0 30px;
+  padding: 60px 0 25px;
   flex-grow: 1;
   &__container {
-    max-width: 700px;
+    max-width: 600px;
     gap: 20px;
-    &-logo {
-      flex: 1 1 0px;
-    }
-    &-description {
-      flex: 1 1 0px;
-      background: $lavender;
-      // border: 4px solid $primary;
-      border-radius: 10px;
-      padding: 20px;
-      opacity: 0;
-      animation: home-appear 1s ease forwards 3.8s;
-      .maj-title {
-        color: $primary;
-        text-align: justify;
-        hyphens: auto;
-
-        &:first-of-type {
-          margin-bottom: auto;
-        }
-      }
-      @keyframes home-appear {
-        to {
-          opacity: 1;
-        }
-      }
+  }
+  .maj-title {
+    color: $secondary;
+    padding-bottom: 5px;
+    border-bottom: 3px solid $secondary;
+  }
+  &__desc {
+    .maj-paragraph {
+      color: $primary;
+      text-align: justify;
+      hyphens: auto;
     }
   }
 }
