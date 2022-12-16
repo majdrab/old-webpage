b60128ccf1e080a3912c778947b72e9afd374268
Maj Drab <maj.drab@gmail.com>
2022-12-16 13:28:31 +0100

    feat: add a data property for defining open-closed state of the menu, change sass so dice stays opened if menu is opened, add menu dropdown component to menu dice

--- src/components/header/MenuDice.vue
+++ src/components/header/MenuDice.vue
@@ -1,19 +1,33 @@
 <template>
     <div class="menu-dice">
-        
-        <MajContainer centerX centerY class="menu-dice__base">
+        <MajContainer 
+            centerX 
+            centerY 
+            :class="['menu-dice__container', {'menu-dice__container--active' : menuOpened}]"
+            @click="menuOpened = !menuOpened"
+        >
             <MenuDiceSvg />
-            <MajIcon class="menu-dice__base-icon" :icon="['fas', 'bars']" />
+            <MajIcon class="menu-dice__container-icon" :icon="['fas', 'bars']" />
         </MajContainer>
+        <MenuDropdown 
+            :menuOpened="menuOpened"
+            @close="menuOpened = false"
+        />
     </div>
 </template>
 
 <script>
 import MenuDiceSvg from "./MenuDiceSvg.vue";
+import MenuDropdown from "./MenuDropdown.vue";
 
 export default {
   name: "MenuDice",
-  components: { MenuDiceSvg }
+  components: { MenuDiceSvg, MenuDropdown },
+  data () {
+    return {
+      menuOpened: false
+    }
+  }
 }
 </script>
 
@@ -23,30 +37,33 @@ export default {
   min-width: 80px;
   height: 80px;
   position: relative;
-  z-index: 2;
+  z-index: 12;
 
-  &__base {
+  &__container {
     min-width: 80px;
     height: 80px;
     position: fixed;
     top: -20px;
+    cursor: pointer;
     transition: all 0.3s;
+    z-index: 12;
+
     &-icon {
       color: $primary;
       font-size: 1.3rem;
       transition: transform 0.3s;
-      z-index: 2;
+      z-index: 12;
     }
-
-    &:hover {
-      transform: translateY(30px) scale(1.2) rotate(60deg);
-      .menu-dice-svg__primary,
-      .menu-dice-svg__secondary {
-        opacity: 1;
-      }
-      .menu-dice__base-icon {
-        transform: rotate(-60deg);
-      }
+  }
+  .menu-dice__container--active,
+  .menu-dice__container:hover {
+    transform: translateY(30px) scale(1.2) rotate(60deg);
+    .menu-dice-svg__primary,
+    .menu-dice-svg__secondary {
+      opacity: 1;
+    }
+    .menu-dice__container-icon {
+      transform: rotate(-60deg);
     }
   }
 }
